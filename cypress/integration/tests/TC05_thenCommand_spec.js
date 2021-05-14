6ef4a2a73b47d5f6860f8c4d22b1b0a2b676437c
Walter Vargas <wvargas@extraspace.com>
2021-05-14 16:14:53 -0600

    08 Using the THEN command

---
+++ cypress/integration/tests/TC05_thenCommand_spec.js
@@ -0,0 +1,35 @@
+describe('Cread and mark-unmark as favorite', function () {
+    
+    Cypress.config('pageLoadTimeout', 10000);
+
+    before(function () {
+        cy.SignIn();
+    });
+
+    it('Create a post', function () {
+        cy.get('ul.navbar-nav').children().contains('New Post').click();
+        cy.hash().should('include', '#/editor');
+        cy.get('form').within(($form) => {
+            cy.get('input').first().type('test title');
+            cy.get('input').eq(1).type('test summary');
+            cy.get('textarea').last().type('test content');
+            cy.contains('Publish Article').click();
+        });
+        cy.url().should('include', 'article');
+    });
+
+    it('Mark-unmark as favorite', function () {
+        cy.get('ul.navbar-nav').children().contains('walter1').click();
+        cy.contains('My Articles').should('be.visible');
+        cy.get('.ion-heart').first().click();
+        cy.contains('Favorited Articles').click();
+        cy.url().should('include', 'favorites');
+        cy.get('.btn-primary').first().then(($fav) =>{
+            const favCount = $fav.text();
+            expect(parseInt(favCount)).to.eq(1);
+        }).click();
+        cy.reload();
+        cy.contains('No articles are here... yet.').should('be.visible');
+        cy.go('back');
+    });
+});
