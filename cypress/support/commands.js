7134afb76e849f96ff16ade6765605c3a7f2e3fc
Walter Vargas <wvargas@extraspace.com>
2021-05-14 15:52:12 -0600

    06 Creating custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,11 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('SignIn', () => {
+    cy.visit('/#/login');
+    cy.title().should('eq', 'Conduit');
+    cy.location('protocol').should('eq', 'https:');
+    cy.get('form').within(($form) => {
+        cy.get('input[type="email"]').type('walter1@gmail.com');
+        cy.get('input[type="password"]').type('12345678');
+        cy.root().submit();
+    });
+    cy.contains('Your Feed', { timeout: 4000 }).should('be.visible');
+})
