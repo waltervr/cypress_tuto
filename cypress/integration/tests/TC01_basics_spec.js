18c8d79081b93224981b38f0c0a2c0e289185cf2
Walter Vargas <wvargas@extraspace.com>
2021-05-14 13:42:25 -0600

    03 Creating the first basic script

---
+++ cypress/integration/tests/TC01_basics_spec.js
@@ -0,0 +1,8 @@
+describe('Login', function(){
+    it('Sign in', function(){
+        cy.visit('https://react-redux.realworld.io/#/login');
+        cy.get('input[type="email"]').type('walter1@gmail.com');
+        cy.get('input[type="password"]').type('12345678');
+        cy.get('.btn').contains('Sign in').should('be.visible').click();
+    });
+});
